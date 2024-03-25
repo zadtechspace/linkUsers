8c3fd1c8b05bbb8e4a668740866dba0ada80104b
Zadtechspace <oppo159350@gmail.com>
2024-03-25 08:41:28 +0100

    first commuit

---
+++ mixir.js
@@ -0,0 +1,17 @@
+const livestream = ()=>{
+
+    herodisp.innerHTML = `<img src="img/mixir/hero-stream-1408-8ab5bb234e57f4a4f130c15e8730b414c25136fa7af090776ee1fbff8acdb23b.png" alt="" style="width: 80%;">`
+
+}
+const host = ()=>{
+
+    herodisp.innerHTML = `<img src="img/mixir/hero-host-1408-f79f1133b4da87d59deac96b35d14949d30e5429d5b480a3f481e1641458b8ad.png" alt="" style="width: 80%;">
+    `
+
+}
+const share = ()=>{
+
+    herodisp.innerHTML = `<img src="img/mixir/hero-share-1408-18979d61a59df1ef8fad20d211af22eb8e6ce44119c7665e9f9a9e22334d5ef5.png" alt="" style="width: 80%;">
+    `
+
+}
